stickyPiston/breakout

commit 969c536e2d09b7af0cc694be832ead3532781aff

🏗  Implemented electron-userland's electron-webpack

.gitignore
@@ -1,6 +1,6 @@
-node_modules
-src/**/*.js
-src/**/*.js.map
-._*
 .DS_Store
-release
+dist/
+node_modules/
+thumbs.db
+.idea/
+._*

src/renderer/screens/gameover/index.ts
@@ -24,11 +24,16 @@ export class GameOverScene extends Container<Text | Rect> {
     pressSpace.pos = { x: 270, y: 275 };
     this.add(pressSpace);
 
-    // Add instructions
-    const points = Score.getInstance().getScore();
+    // TODO: Calculate score like it's done in multiplayer mode
+		const points = Score.getInstance().getScore();
     const scoreText = new Text(`SCORE: ${points}`, { font: "24px forced", fill: "#fff" });
     scoreText.pos = { x: 330, y: 200 };
     this.add(scoreText);
+
+		// TODO: Add highscore to database
+		const highscores = JSON.parse(readFileSync(highscorePath).toString());
+		highscores.push({ score: points, date: Date.now(), type: "singleplayer" });
+		writeFileSync(highscorePath, JSON.stringify(highscores));
 
 		Timer.getInstance().resetTimer();
   }

src/renderer/screens/connecting/index.ts
@@ -30,7 +30,7 @@ export class SearchingScene extends Container<Text | Rect> {
 		SearchingScene.instance.opponentFound = false;
 		SearchingScene.instance.countdown = 3;
 		SearchingScene.instance.period = 0.75;
-		SearchingScene.instance.children[1].text = "Searching for an opponent...";
+		(SearchingScene.instance.children[1] as Text).text = "Searching for an opponent...";
 
 		if (!socket.connected) socket.open();
 		socket.emit("identification", { gameID: "brek", playersMax: 2 });
@@ -51,8 +51,8 @@ export class SearchingScene extends Container<Text | Rect> {
 		if (this.opponentFound) {
 			this.countdown -= dt;
 
-			this.children[1].style.fill = "#fff";
-			this.children[1].text = "Opponent Found! Starting game in " + Math.ceil(this.countdown);
+			(this.children[1] as Text).style.fill = "#fff";
+			(this.children[1] as Text).text = "Opponent Found! Starting game in " + Math.ceil(this.countdown);
 			this.children[1].pos.x = 230;
 		} else {
     	// Periodically toggle text

src/renderer/screens/game/index.ts
@@ -4,7 +4,6 @@ import { Ball } from "./ball";
 import { Score } from "./helpers/score";
 import { Lives } from "./helpers/lives";
 import { Level } from "./helpers/level";
-import { keys } from "../../constants";
 import { SceneManager } from "../../scenemanager";
 import { PowerupManager } from "./helpers/powerup";
 import { Blocks } from "./blocks";

src/renderer/screens/mainmenu/index.ts
@@ -18,24 +18,24 @@ export class MainMenu extends Container<Rect | Text> {
 
     // Add title
     const title = new Text("BREAKOUT", { font: "24px pixelmania", fill: "#fff" });
-    title.pos = { x: (800 - 300) / 2, y: 125 };
+    title.pos = { x: (800 - 300) / 2, y: 75 };
     this.add(title);
 
     // Add press space to start text
     const pressS = new Text("> Play singleplayer mode", { font: "24px forced", fill: "#fff" });
-    pressS.pos = { x: (800 - 270) / 2, y: 200 };
+    pressS.pos = { x: (800 - 270) / 2, y: 150 };
     this.add(pressS);
 
     const pressM = new Text("Play multiplayer mode", { font: "24px forced", fill: "#fff" });
-    pressM.pos = { x: (800 - 243) / 2, y: 240 };
+    pressM.pos = { x: (800 - 243) / 2, y: 190 };
     this.add(pressM);
 
     const pressH = new Text("Help", { font: "24px forced", fill: "#fff" });
-    pressH.pos = { x: (800 - 70) / 2, y: 280};
+    pressH.pos = { x: (800 - 70) / 2, y: 230};
     this.add(pressH);
 
     const pressT = new Text("Settings", { font: "24px forced", fill: "#fff" });
-    pressT.pos = { x: (800 - 120) / 2, y: 320};
+    pressT.pos = { x: (800 - 120) / 2, y: 270};
     this.add(pressT);
 
     const credit = new Text("This game is made by Job Vonk", { font: "18px forced", fill: "#fff" });
@@ -92,7 +92,7 @@ export class MainMenu extends Container<Rect | Text> {
       else if (this.selectedIndex === 2)
         SceneManager.getInstance().setScene(1);
 			else if (this.selectedIndex === 3)
-				SceneManager.getInstance().setScene(10);
+				SceneManager.getInstance().setScene(8);
     }
 
     // Periodically toggle text

src/renderer/screens/results/index.ts
@@ -78,8 +78,6 @@ export class ResultsScene extends Container<Rect | Text> {
 			SceneManager.getInstance().setScene(0);
 		}
 
-		// TODO: Add fancy animations and fix styling
-
 		if (this.children[7]) {
 			this.period -= dt;
 

commit 1583de6c482b856227b24340ac6474cd5bd5edb4

👌 Updated some files that caused error after 969c536

.gitignore
@@ -4,3 +4,4 @@ node_modules/
 thumbs.db
 .idea/
 ._*
+release

src/renderer/screens/gameover/index.ts
@@ -31,9 +31,6 @@ export class GameOverScene extends Container<Text | Rect> {
     this.add(scoreText);
 
 		// TODO: Add highscore to database
-		const highscores = JSON.parse(readFileSync(highscorePath).toString());
-		highscores.push({ score: points, date: Date.now(), type: "singleplayer" });
-		writeFileSync(highscorePath, JSON.stringify(highscores));
 
 		Timer.getInstance().resetTimer();
   }